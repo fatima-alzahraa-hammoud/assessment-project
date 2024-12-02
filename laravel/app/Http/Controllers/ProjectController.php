bcf1b951ba2124e50930c8003b41769d9d77b4bf
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-02 11:47:34 +0200

    Laravel <> Create - ProjectController with get methods

---
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+
+    // get all projects
+    public function getAllProjects(){
+        $projects = Project::all();
+
+        return response()->json([
+            "projects"=> $projects
+        ]);
+    }
+
+    // get all projects with their members
+    public function getProjectsWithMembers(){
+        // joining projects with members table based on the project id, and then with users based on user id to get the name, this query returns a lot of queries with a lot of projects ids with different members id
+        $projects = Project::join('members','projects.id', '=', 'members.project_id')
+                    ->join('users', 'users.id', '=', 'members.user_id')
+                    ->select('projects.id as id', 'projects.name as name', 'projects.description as description', 'users.name as member_name')
+                    ->get();
+
+        //$allProjects = [];
+        // now i need to get the projects with the list of members name inside it
+        /*$groupingProjects = $projects->groupBy("id")->map(function($project) {
+            return [
+                "id" => $project->id,
+                "name" => $project->name,
+                "description" => $project->description,
+                "members"
+            ]
+        })*/ 
+        
+        return response()->json([
+            'projects'=> $projects,
+        ]);
+    }
+}
